2b1768948a5839634103aa63d6182bed9933f07b
howserstrousers <fritzcharleshowser@gmail.com>
2016-10-01 23:40:29 -0400

    parallax

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+	$(document).ready(function() {
+
+		$('.proj').parallax({
+	      invertX: false,
+	      invertY: false,
+	      scalarX:10,
+	      scalarY:10,
+	      originX:1,
+	      originY:0,
+	    });
+	/*
+	    $('#project2').parallax({
+	      invertX: true,
+	      invertY: false,
+	      scalarX:100,
+	      scalarY: 100,
+	      originX:0,
+	      originY:.5,
+	      calibrateX:true,
+	      calibrateY:true,
+	    });
+
+	    $('#project3').parallax({
+	      RelativeInput:true,
+	      invertX: false,
+	      invertY: false,
+	      scalarX:100,
+	      scalarY: 150,
+	      originX:1,
+	      originY:0,
+	      calibrateX:true,
+	      calibrateY:true,
+	    });
+	*/
+
+	})
+}
