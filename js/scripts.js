199e2820b6c54d3a775e30e6694d92076237a775
howserstrousers <fritzcharleshowser@gmail.com>
2016-10-03 19:16:14 -0400

    nav mar centered

--- js/scripts.js
+++ js/scripts.js
@@ -9,32 +9,7 @@ window.onload = function() {
 	      originX:.5,
 	      originY:.5,
 	    });
-	/*
-	    $('#project2').parallax({
-	      invertX: true,
-	      invertY: false,
-	      scalarX:100,
-	      scalarY: 100,
-	      originX:0,
-	      originY:.5,
-	      calibrateX:true,
-	      calibrateY:true,
-	    });
-
-	    $('#project3').parallax({
-	      RelativeInput:true,
-	      invertX: false,
-	      invertY: false,
-	      scalarX:100,
-	      scalarY: 150,
-	      originX:1,
-	      originY:0,
-	      calibrateX:true,
-	      calibrateY:true,
-	    });
-	*/
-
 	});
 
-	
+
 }
